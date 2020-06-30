74fc6a2402b8947f968e5a9939332a7a2f5390b4
FlorianSdorra <fsd114@posteo.de>
2020-06-30 14:33:11 +0200

    created all pages / using Link to redirect

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AboutPage = () => {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>Here are some details going as well.</p>
+        </div>
+        
+    )
+}
+
+export default AboutPage;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,13 @@
-import React from "react"
+import React from "react";
+import {Link} from "gatsby";
 
 const IndexPage = () => {
   return (
     <div>
       <h1>Bonjourno.</h1>
       <h2>I'm Flo,  a full stack developer living in beautiful Berlin.</h2>
+      <p>Need a developer? <a href="/contact">Contact me.</a></p>
+      <p>Need a developer? <Link to="/contact">Contact me.</Link></p>
     </div>
   )
 }
