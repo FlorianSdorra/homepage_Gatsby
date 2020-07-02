6c33ffd39d78cec4b786e3c1b3da1a9d89b37f9c
FlorianSdorra <fsd114@posteo.de>
2020-07-02 15:05:22 +0200

    finished first part of styling for now

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,6 @@ const IndexPage = () => {
     <Layout>
       <h1>Bonjourno.</h1>
       <h2>I'm Flo,  a full stack developer living in beautiful Berlin.</h2>
-      <p>Need a developer? <a href="/contact">Contact me.</a></p>
       <p>Need a developer? <Link to="/contact">Contact me.</Link></p>
     </Layout>
   )
