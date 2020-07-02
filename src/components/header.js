746d247a32e140951cdb3dbd798b38b2044c71e7
FlorianSdorra <fsd114@posteo.de>
2020-07-02 13:55:35 +0200

    some raw styling and layouting

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import {Link} from 'gatsby';
+import headerStyles from './header.module.scss';
 
 const Header = () => {
     return (
-        <header>
-            <h1>Florian Sdorra</h1>
+        <header className={headerStyles.header}>
+            <h1>
+                <Link to="/">Florian Sdorra</Link>
+            </h1>
             <nav>
                 <ul>
-                    <li><Link to="/">Home</Link> </li>
+                    <li><Link className={headerStyles.link} to="/">Home</Link> </li>
                     <li><Link to="/blog">Blog</Link></li>
                     <li><Link to="/about">About</Link></li>
                     <li><Link to="/contact">Contact</Link></li>
