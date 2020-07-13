8dbbddf4cd35dbb050cbb3339de0cbdf524602fa
FlorianSdorra <fsd114@posteo.de>
2020-07-13 10:45:13 +0200

    rendering posts in blog.js

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,11 +1,39 @@
 import React from 'react';
 import Layout from '../components/layout';
+import {graphql, useStaticQuery} from 'gatsby';
 
 const BlogPage = ()=> {
+
+    const data = useStaticQuery(graphql`
+    query{
+        allMarkdownRemark {
+            edges {
+                node {
+                    frontmatter {
+                        title
+                        date
+                    }
+                }
+            }
+        }
+    }
+`)
+
+    const posts = data.allMarkdownRemark.edges.map(el=>
+        <li>
+            <h2>{el.node.frontmatter.title}</h2>
+            <p>{el.node.frontmatter.date}</p>
+        </li>
+        
+    )
+
     return(
         <Layout>
             <h1>Blog</h1>
             <p>Post will show up here late on.</p>
+            <ol>
+                {posts}
+            </ol>
         </Layout>
     )
 }
