834b4d6887cf6af4244eceab255891a07db7589d
FlorianSdorra <fsd114@posteo.de>
2020-08-12 16:05:28 +0200

    setting up contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,14 @@ module.exports = {
     author: 'Florian Sdorra'
   },
   plugins: [
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        // Learn about environment variables: https://gatsby.dev/env-vars
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+      },
+    },
     'gatsby-plugin-sass',
     {
       resolve: `gatsby-source-filesystem`,
