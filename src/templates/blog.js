f355108c1e9432110a4a8dfe46ae8b39f70f9050
FlorianSdorra <fsd114@posteo.de>
2020-08-18 21:22:45 +0200

    changed node.js to create pages from contentful content & blog template to render content

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -3,34 +3,42 @@ import { graphql } from 'gatsby';
 
 import Layout from '../components/layout';
 
+// export const query = graphql`
+//     query (
+//         $slug: String!
+//     ){
+//         markdownRemark (
+//         fields: {
+//             slug: {
+//             eq: $slug
+//             }
+//         }
+//         ) 
+//         {
+//         frontmatter{
+//             title
+//             date
+//         }
+//         html
+//         }
+//     } 
+// `
+
 export const query = graphql`
-    query (
-        $slug: String!
-    ){
-        markdownRemark (
-        fields: {
-            slug: {
-            eq: $slug
-            }
-        }
-        ) 
-        {
-        frontmatter{
-            title
-            date
-        }
-        html
-        }
-    } 
+query($slug:String!){
+    contentfulBlogPost(slug: {eq:$slug}){
+        title
+        publishedDate(formatString: "MMMM Do, YYYY")
+    }
+  }
 `
 
 const Blog = (props) => {
     console.log(props)
     return (
         <Layout>
-            <h1>{props.data.markdownRemark.frontmatter.title}</h1>
-            <p>{props.data.markdownRemark.frontmatter.date}</p>
-            <div dangerouslySetInnerHTML={{__html:props.data.markdownRemark.html}}></div>
+            <h1>{props.data.contentfulBlogPost.title}</h1>
+            <p>{props.data.contentfulBlogPost.publishedDate}</p>
         </Layout>
     )
 }
