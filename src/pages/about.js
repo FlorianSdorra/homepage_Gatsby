a11c3d55db2895371aa02c9a6cc1e8df5bd77408
FlorianSdorra <fsd114@posteo.de>
2020-09-14 18:01:06 +0200

    foundation for about is done

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import Typed from 'react-typed';
 
 import Layout from '../components/layout';
-import Head from '../components/head'
+import Head from '../components/head';
+
+import aboutStyles from './about.module.scss';
 
 const AboutPage = () => {
     return (
         <Layout>
             <Head title="About"/>
-            <h1>About</h1>
-            <p>Here are some details going as well.</p>
-            <p>Need a developer? <Link to="/contact">Contact me.</Link></p>
+            <div className={aboutStyles.container}>
+                <div className={aboutStyles.intro}>
+                    <h1>About</h1>
+                    <p className={`${aboutStyles.inner} ${aboutStyles.typefix}`}>
+                        <Typed
+                        strings={[
+                        'I have to write an interesting text about myself which magnifies HR people'
+                        ]}
+                        typeSpeed={50}
+                 />
+                    </p>
+                </div>
+                
+            </div>
         </Layout>
         
     )
